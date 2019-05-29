579dca5b10f463c6e7acc93f222c0c5410cf7507
shamil@redlabs.tech <shamil@redlabs.tech>
2019-05-29 21:11:08 +0530

    Added route for post user & added validation module

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,29 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getUser = (req, res, next) => {
     res.status(200).json({
-        posts: [{ name: 'Shamil', email: 'shamal@shamil.com' }]
+        posts: [{ username: 'Shamil', email: 'shamal@shamil.com' }]
+    })
+}
+
+exports.postUser = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+        .status(422)
+        .json(
+            {message: 'Validation failed. Entered user details is incorrect', errors: errors.array()
+        })
+    }
+    const username = req.body.username;
+    const email = req.body.email;
+
+    res.status(200).json({
+        message: 'User saved successfully',
+        post: {
+            _id: new Date().toISOString(),
+            username: username,
+            email: email
+        }
     })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator/check')
 
 const authController = require('../controllers/auth')
 
@@ -6,5 +7,8 @@ const router = express.Router()
 
 //GET => /auth/user
 router.get('/user', authController.getUser)
+
+//POST => /auth/user
+router.post('/user', authController.postUser)
 
 module.exports = router;
