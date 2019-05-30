ade9c67c7354c9984313b54b50a8738faec74db6
shamil@redlabs.tech <shamil@redlabs.tech>
2019-05-30 21:52:01 +0530

    Added validation to check if the user exists

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { body } = require('express-validator/check')
 
+const User = require('../models/user')
+
 const authController = require('../controllers/auth')
 
 const router = express.Router()
@@ -12,7 +14,17 @@ router.get('/user', authController.getUser)
 router.post('/user', [
     body('name').trim().isLength({ min: 5 }),
     body('password').trim().isLength({ min: 3 }),
-    body('email').isEmail().withMessage('Please enter valid email').normalizeEmail()
+    body('email')
+        .isEmail()
+        .withMessage('Please enter valid email')
+        .normalizeEmail()
+        .custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('Account already exists with the given email address')
+                }
+            })
+        })
 ],
     authController.postUser
 )
