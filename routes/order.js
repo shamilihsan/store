d93159d64d2139055cf9ca425278eb541cabe6a4
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-06-21 15:43:06 +0530

    Added getorders to routes

--- routes/order.js
+++ routes/order.js
@@ -7,4 +7,7 @@ const router = express.Router()
 //POST => /order/add-order {email, items, total}
 router.post('/add-order', orderController.addOrder)
 
+//POST => /order/add-order {userId}
+router.post('/get-orders', orderController.getOrders)
+
 module.exports = router
