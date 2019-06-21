c0428da18959d99725c6d086dfe7499dda7759b9
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-06-21 13:56:45 +0530

    Added addOrder logic

--- controllers/order.js
+++ controllers/order.js
@@ -1,10 +1,40 @@
 const Order = require('../models/order')
+const User = require('../models/user')
 
 exports.addOrder = (req, res, next) => {
-    
+
     const email = req.body.email
     const items = req.body.items
     const total = req.body.total
+
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with the email address ' + email + ' does not exist')
+                error.statusCode = 422
+                error.data = errors.array()
+                throw error
+            }
+
+            const order = new Order({
+                items: items,
+                user: {
+                    email: email,
+                    userId: user
+                }
+            })
+
+            return order.save()
+        })
+        .then(result => {
+            res.status(201).json({ message: 'Added order successfully', order: result })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
 
     console.log(email, items, total);
 
