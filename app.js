df4ab4bdcaf6cf541c0a68da10f4dafada3e4e58
Shamil Ihsan <shamil.ihsan@hotmail.com>
2019-09-08 17:56:03 +0530

    Improved cors headers

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader(
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
       'Access-Control-Allow-Methods',
       'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
